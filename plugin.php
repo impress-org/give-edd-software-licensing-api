690a1e7ab504671bd7275c4f208b24199d56392f
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2019-04-29 18:15:46 +0530

    feat: copy required fn from EDD_Software_Licensing to reduce response time
    when do bulk license check

--- plugin.php
+++ plugin.php
@@ -76,7 +76,6 @@ class Give_EDD_Software_Licensing_API_Extended {
 	 * @since   0.1
 	 * @access  public
 	 * @wp-hook plugins_loaded
-	 *
 	 */
 	public static function get_instance() {
 		if ( null === static::$instance ) {
@@ -93,12 +92,245 @@ class Give_EDD_Software_Licensing_API_Extended {
 	 * @since   0.1
 	 * @access  public
 	 * @wp-hook plugins_loaded
-	 *
 	 */
 	public function plugin_setup() {
-		add_filter( 'edd_remote_license_check_response', array( $this, 'remote_license_check' ), 10, 3 );
+		add_filter( 'edd_remote_license_check_response', array( $this, 'additional_license_checks' ), 10, 3 );
 		add_action( 'edd_check_subscription', array( $this, 'remote_subscription_check' ) );
 		add_action( 'edd_check_licenses', array( $this, 'remote_licenses_check' ) );
+	}
+
+	/**
+	 * Given an array of arguments, sort them by length, and then md5 them to generate a checksum.
+	 * Note: this function copied from EDD_Software_Licensing to descrease response time when do bulk licenses check
+	 *
+	 * @param array $args
+	 *
+	 * @return string
+	 * @since 3.5
+	 */
+	private function get_request_checksum_copy( $args = array() ) {
+		usort( $args, array( $this, 'sort_args_by_length' ) );
+		$string_args = json_encode( $args );
+
+		return md5( $string_args );
+	}
+
+	/**
+	 * License check
+	 * Note: this function copied from EDD_Software_Licensing to descrease response time when do bulk licenses check
+	 *
+	 * Modification list:
+	 * 1. return array instead of json
+	 *
+	 * @param array $data
+	 *
+	 * @return array
+	 */
+	function remote_license_check_copy( $data ) {
+
+		$item_id   = ! empty( $data['item_id'] ) ? absint( $data['item_id'] ) : false;
+		$item_name = ! empty( $data['item_name'] ) ? rawurldecode( $data['item_name'] ) : false;
+		$license   = isset( $data['license'] ) ? urldecode( $data['license'] ) : false;
+		$url       = isset( $data['url'] ) ? urldecode( $data['url'] ) : '';
+
+		$args = array(
+			'item_id'   => $item_id,
+			'item_name' => $item_name,
+			'key'       => $license,
+			'url'       => $url,
+		);
+
+		$result   = $message = edd_software_licensing()->check_license( $args );
+		$checksum = $this->get_request_checksum_copy( $args );
+
+		if ( 'invalid' === $result ) {
+			$result = false;
+		}
+
+		$response = array();
+		if ( false !== $result ) {
+			$license = edd_software_licensing()->get_license( $license, true );
+
+			$response['expires']          = is_numeric( $license->expiration ) ? date( 'Y-m-d H:i:s', $license->expiration ) : $license->expiration;
+			$response['payment_id']       = $license->payment_id;
+			$response['customer_name']    = $license->customer->name;
+			$response['customer_email']   = $license->customer->email;
+			$response['license_limit']    = $license->activation_limit;
+			$response['site_count']       = $license->activation_count;
+			$response['activations_left'] = $license->activation_limit > 0 ? $license->activation_limit - $license->activation_count : 'unlimited';
+			$response['price_id']         = $license->price_id;
+		}
+
+		if ( empty( $item_name ) ) {
+			$item_name = get_the_title( $item_id );
+		}
+
+		$response = array_merge(
+			array(
+				'success'   => (bool) $result,
+				'license'   => $message,
+				'item_id'   => $item_id,
+				'item_name' => $item_name,
+				'checksum'  => $checksum,
+			),
+			$response
+		);
+
+		$license_id = ! empty( $license->ID ) ? $license->ID : false;
+
+		return apply_filters( 'edd_remote_license_check_response', $response, $args, $license_id );
+	}
+
+	/**
+	 * Version Check
+	 * Note: this function copied from EDD_Software_Licensing to descrease response time when do bulk licenses check
+	 *
+	 * Modification list:
+	 * 1. return array instead of json
+	 *
+	 * @param array $data
+	 *
+	 * @return array
+	 */
+	function get_latest_version_remote_copy( $data ) {
+
+		$url       = isset( $data['url'] ) ? sanitize_text_field( urldecode( $data['url'] ) ) : false;
+		$license   = isset( $data['license'] ) ? sanitize_text_field( urldecode( $data['license'] ) ) : false;
+		$slug      = isset( $data['slug'] ) ? sanitize_text_field( urldecode( $data['slug'] ) ) : false;
+		$item_id   = isset( $data['item_id'] ) ? absint( $data['item_id'] ) : false;
+		$item_name = isset( $data['item_name'] ) ? sanitize_text_field( rawurldecode( $data['item_name'] ) ) : false;
+		$beta      = isset( $data['beta'] ) ? (bool) $data['beta'] : false;
+		if ( empty( $item_name ) && empty( $item_id ) ) {
+			$item_name = isset( $data['name'] ) ? sanitize_text_field( rawurldecode( $data['name'] ) ) : false;
+		}
+
+		$response = array(
+			'new_version'    => '',
+			'stable_version' => '',
+			'sections'       => '',
+			'license_check'  => '',
+			'msg'            => '',
+		);
+
+		if ( empty( $item_id ) && empty( $item_name ) && ( ! defined( 'EDD_BYPASS_NAME_CHECK' ) || ! EDD_BYPASS_NAME_CHECK ) ) {
+			$response['msg'] = __( 'No item provided', 'edd_sl' );
+
+			return $response;
+		}
+
+		if ( empty( $item_id ) ) {
+
+			if ( empty( $license ) && empty( $item_name ) ) {
+				$response['msg'] = __( 'No item provided', 'edd_sl' );
+				return $response;
+			}
+
+			$check_by_name_first = apply_filters( 'edd_sl_force_check_by_name', false );
+
+			if ( empty( $license ) || $check_by_name_first ) {
+
+				$item_id = edd_software_licensing()->get_download_id_by_name( $item_name );
+
+			} else {
+
+				$item_id = edd_software_licensing()->get_download_id_by_license( $license );
+
+			}
+		}
+
+		$download = new EDD_SL_Download( $item_id );
+
+		if ( ! $download ) {
+
+			if ( empty( $license ) || $check_by_name_first ) {
+				$response['msg'] = sprintf( __( 'Item name provided does not match a valid %s', 'edd_sl' ), edd_get_label_singular() );
+			} else {
+				$response['msg'] = sprintf( __( 'License key provided does not match a valid %s', 'edd_sl' ), edd_get_label_singular() );
+			}
+
+			return $response;
+		}
+
+		$is_valid_for_download = edd_software_licensing()->is_download_id_valid_for_license( $download->ID, $license );
+		if ( ! empty( $license ) && ( ! defined( 'EDD_BYPASS_NAME_CHECK' ) || ! EDD_BYPASS_NAME_CHECK ) && ( ! $is_valid_for_download || ( ! empty( $item_name ) && ! edd_software_licensing()->check_item_name( $download->ID, $item_name, $license ) ) ) ) {
+
+			$download_name   = ! empty( $item_name ) ? $item_name : $download->get_name();
+			$response['msg'] = sprintf( __( 'License key is not valid for %s', 'edd_sl' ), $download_name );
+
+			return $response;
+		}
+
+		$stable_version = $version = edd_software_licensing()->get_latest_version( $item_id );
+		$slug           = ! empty( $slug ) ? $slug : $download->post_name;
+		$description    = ! empty( $download->post_excerpt ) ? $download->post_excerpt : $download->post_content;
+		$changelog      = $download->get_changelog();
+
+		$download_beta = false;
+		if ( $beta && $download->has_beta() ) {
+			$version_beta = edd_software_licensing()->get_beta_download_version( $item_id );
+			if ( version_compare( $version_beta, $stable_version, '>' ) ) {
+				$changelog     = $download->get_beta_changelog();
+				$version       = $version_beta;
+				$download_beta = true;
+			}
+		}
+
+		$response = array(
+			'new_version'    => $version,
+			'stable_version' => $stable_version,
+			'name'           => $download->post_title,
+			'slug'           => $slug,
+			'url'            => esc_url( add_query_arg( 'changelog', '1', get_permalink( $item_id ) ) ),
+			'last_updated'   => $download->post_modified,
+			'homepage'       => get_permalink( $item_id ),
+			'package'        => edd_software_licensing()->get_encoded_download_package_url( $item_id, $license, $url, $download_beta ),
+			'download_link'  => edd_software_licensing()->get_encoded_download_package_url( $item_id, $license, $url, $download_beta ),
+			'sections'       => serialize(
+				array(
+					'description' => wpautop( strip_tags( $description, '<p><li><ul><ol><strong><a><em><span><br>' ) ),
+					'changelog'   => wpautop( strip_tags( stripslashes( $changelog ), '<p><li><ul><ol><strong><a><em><span><br>' ) ),
+				)
+			),
+			'banners'        => serialize(
+				array(
+					'high' => get_post_meta( $item_id, '_edd_readme_plugin_banner_high', true ),
+					'low'  => get_post_meta( $item_id, '_edd_readme_plugin_banner_low', true ),
+				)
+			),
+			'icons'          => array(),
+		);
+
+		if ( has_post_thumbnail( $download->ID ) ) {
+			$thumb_id  = get_post_thumbnail_id( $download->ID );
+			$thumb_128 = get_the_post_thumbnail_url( $download->ID, 'sl-small' );
+			if ( ! empty( $thumb_128 ) ) {
+				$response['icons']['1x'] = $thumb_128;
+			}
+
+			$thumb_256 = get_the_post_thumbnail_url( $download->ID, 'sl-large' );
+			if ( ! empty( $thumb_256 ) ) {
+				$response['icons']['2x'] = $thumb_256;
+			}
+		}
+
+		$response['icons'] = serialize( $response['icons'] );
+
+		$response = apply_filters( 'edd_sl_license_response', $response, $download, $download_beta );
+
+		/**
+		 * Encode any emoji in the name and sections.
+		 *
+		 * @since 3.6.5
+		 * @see   https://github.com/easydigitaldownloads/EDD-Software-Licensing/issues/1313
+		 */
+		if ( function_exists( 'wp_encode_emoji' ) ) {
+			$response['name'] = wp_encode_emoji( $response['name'] );
+
+			$sections             = maybe_unserialize( $response['sections'] );
+			$response['sections'] = serialize( array_map( 'wp_encode_emoji', $sections ) );
+		}
+
+		return $response;
 	}
 
 
@@ -109,8 +341,8 @@ class Give_EDD_Software_Licensing_API_Extended {
 	 */
 	public function remote_licenses_check( $args ) {
 		$defaults = array(
-			'licenses'  => '',
-			'url'       => '',
+			'licenses' => '',
+			'url'      => '',
 		);
 
 		$args             = array_map( 'sanitize_text_field', $args );
@@ -135,70 +367,47 @@ class Give_EDD_Software_Licensing_API_Extended {
 					'get_versions'  => array(),
 				);
 
-				$remote_response = wp_remote_post(
-					home_url( '/checkout/' ),
+				$remote_response = $this->remote_license_check_copy(
 					array(
-						'timeout'   => 15,
-						'sslverify' => false,
-						'body'      => array(
-							'edd_action' => 'check_license',
-							'license'    => $license,
-							'url'        => $args['url'],
-						),
+						'edd_action' => 'check_license',
+						'license'    => $license,
+						'url'        => $args['url'],
 					)
 				);
 
-				$response[ $license ]['check_license'] = ! is_wp_error( $remote_response )
-					? json_decode( wp_remote_retrieve_body( $remote_response ), true )
-					: $remote_response;
+				$response[ $license ]['check_license'] = $remote_response;
 
 				if ( $response[ $license ]['check_license']['success'] ) {
-					if( $response[ $license ]['check_license']['is_all_access_pass'] ){
+					if ( $response[ $license ]['check_license']['is_all_access_pass'] ) {
 						foreach ( $response[ $license ]['check_license']['download'] as $download ) {
-							$remote_response = wp_remote_post(
-								home_url( '/checkout/' ),
+							$remote_response = $this->get_latest_version_remote_copy(
 								array(
-									'timeout'   => 15,
-									'sslverify' => false,
-									'body'      => array(
-										'edd_action' => 'get_version',
-										'license'    => $license,
-										'url'        => $args['url'],
-										'item_name'  => $download['name'],
-										'slug'       => $download['plugin_slug'],
-									)
-								)
-							);
-
-							$response[ $license ]['get_versions'][] = ! is_wp_error( $remote_response )
-								? json_decode( wp_remote_retrieve_body( $remote_response ), true )
-								: $remote_response;
-						}
-
-					} else{
-						$remote_response = wp_remote_post(
-							home_url( '/checkout/' ),
-							array(
-								'timeout'   => 15,
-								'sslverify' => false,
-								'body'      => array(
 									'edd_action' => 'get_version',
 									'license'    => $license,
 									'url'        => $args['url'],
-									'item_name'  => $response[ $license ]['check_license']['item_name'],
-									'slug'       => $response[ $license ]['check_license']['plugin_slug'],
-								),
+									'item_name'  => $download['name'],
+									'slug'       => $download['plugin_slug'],
+								)
+							);
+
+							$response[ $license ]['get_versions'][] = $remote_response;
+						}
+					} else {
+						$remote_response = $this->get_latest_version_remote_copy(
+							array(
+								'edd_action' => 'get_version',
+								'license'    => $license,
+								'url'        => $args['url'],
+								'item_name'  => $response[ $license ]['check_license']['item_name'],
+								'slug'       => $response[ $license ]['check_license']['plugin_slug'],
 							)
 						);
 
-						$response[ $license ]['get_version'] = ! is_wp_error( $remote_response )
-							? json_decode( wp_remote_retrieve_body( $remote_response ), true )
-							: $remote_response;
+						$response[ $license ]['get_version'] = $remote_response;
 					}
 				}
 			}
 		}
-
 
 		header( 'Content-Type: application/json' );
 		echo wp_json_encode( $response );
@@ -218,9 +427,8 @@ class Give_EDD_Software_Licensing_API_Extended {
 	 * @since 0.3
 	 *
 	 * @todo  : return result only if source set to give or something else because we do not want to sent extra information on every license check
-	 *
 	 */
-	public function remote_license_check( $response, $args, $license_id ) {
+	public function additional_license_checks( $response, $args, $license_id ) {
 		// @todo: decide whether all access pass can be varibale priced if yes then how it will impect code..
 		// @todo: decide whether send this addition license data to only add-ons page of Give core.
 		// @todo discuss with devin that do we agree to deactivate license if it this request will come from non registered site
@@ -256,18 +464,20 @@ class Give_EDD_Software_Licensing_API_Extended {
 			if ( ! empty( $all_access_pass->all_access_meta['all_access_categories'] ) ) {
 				$response['download'] = array();
 
-				$included_downloads = new WP_Query( array(
-					'post_type'      => 'download',
-					'post_status'    => 'publish',
-					'tax_query'      => array(
-						array(
-							'taxonomy' => 'download_category',
-							'field'    => 'term_id',
-							'terms'    => $all_access_pass->all_access_meta['all_access_categories'],
+				$included_downloads = new WP_Query(
+					array(
+						'post_type'      => 'download',
+						'post_status'    => 'publish',
+						'tax_query'      => array(
+							array(
+								'taxonomy' => 'download_category',
+								'field'    => 'term_id',
+								'terms'    => $all_access_pass->all_access_meta['all_access_categories'],
+							),
 						),
-					),
-					'posts_per_page' => - 1,
-				) );
+						'posts_per_page' => - 1,
+					)
+				);
 
 				// print_r( $included_downloads );
 
@@ -337,7 +547,6 @@ class Give_EDD_Software_Licensing_API_Extended {
 	 *
 	 * @return array
 	 * @since 0.3
-	 *
 	 */
 	private function get_latest_release( $download_files ) {
 		if ( empty( $download_files ) ) {
@@ -378,7 +587,6 @@ class Give_EDD_Software_Licensing_API_Extended {
 	 * @return array|object|null Subscription data
 	 * @since  0.1
 	 * @access public
-	 *
 	 */
 	function get_subscription( $payment_id ) {
 		global $wpdb;
@@ -446,7 +654,6 @@ class Give_EDD_Software_Licensing_API_Extended {
 	 * @return void
 	 * @since  0.1
 	 * @access public
-	 *
 	 */
 	function remote_subscription_check( $data ) {
 		$license = edd_software_licensing()->get_license( urldecode( $data['license'] ), true );
